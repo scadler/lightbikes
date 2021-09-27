8117111690524bb4abb8b0ea8a2e373d40fd3c8c
scadler <scotthadler01@gmail.com>
2021-09-27 23:29:34 +0000

    collsion detection now working

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var bike = {
     diry: 0,
     jump: 0,
     jumpCooldown: 0,
+    dead: false,
 }
 function draw(x, y, dirx, diry, jump) {
     ctx.strokeStyle = (jump === 0) ? "#FFFFFF" :"#909090";
@@ -18,32 +19,32 @@ function draw(x, y, dirx, diry, jump) {
     ctx.moveTo(x - dirx*2/3, y - diry*2/3);
     ctx.lineTo(x + dirx, y + diry);
     ctx.stroke()
-    let dx = (dirx === 0) ? 1 : dirx*4/3;
-    let dy = (diry === 0) ? 1 : diry*4/3;
-    let cc = (diry >0) ? 3 : (diry < 0) ? -4 : 0;
-    let dd = (dirx >0) ? 3 : (dirx < 0) ? -4 : 0;
-
-    ctx.fillStyle = "#FF0000"
-    ctx.fillRect(x+dd-Math.ceil(cc/2),y+cc-Math.ceil(dd/2),dy,dx)
+   
 }
 function move(){
     let height = (bike.jump !==0)? Math.round((8-(bike.jump-2))/4) : 0;
     bike.x+=bike.dirx + (height*Math.abs(bike.diry/3));
     bike.y+=bike.diry + (height*Math.abs(bike.dirx/3));
 }
-function collisionDetect(x,y,dirx,diry){
-    let dx = (dirx === 0) ? 1 : dirx;
-    let dy = (diry === 0) ? 1 : diry;
-    console.log(ctx.getImageData(x, y, dx, dy).data)
-    if(ctx.getImageData(x, y, dx, dy).data.includes(255)){
-        console.log("wsiwebfripweufyp")
+function collisionDetect(x,y,dirx,diry,jump){
+    if(jump===0){
+    let dx = (dirx === 0) ? 1 : dirx*4/3;
+    let dy = (diry === 0) ? 1 : diry*4/3;
+    let cc = (diry >0) ? 3 : (diry < 0) ? -4 : 0;
+    let dd = (dirx >0) ? 3 : (dirx < 0) ? -4 : 0;
+    console.log(ctx.getImageData(x+dd-Math.ceil(cc/2), y+cc-Math.ceil(dd/2), dy, dx).data.filter(function(value) {return value ===255; }).length)
+    if(ctx.getImageData(x+dd-Math.ceil(cc/2), y+cc-Math.ceil(dd/2), dy, dx).data.filter(function(value) {return value ===255; }).length >= 5){
+        console.log("white")
+        bike.dead = true;
+        }
     }
 }
 
 function step(){
+    if(bike.dead == false){
     draw(bike.x,bike.y,bike.dirx,bike.diry,bike.jump)
     move()
-    collisionDetect(bike.x,bike.y,bike.dirx,bike.diry)
+    collisionDetect(bike.x,bike.y,bike.dirx,bike.diry,bike.jump)
     if(bike.jump !== 0){
         bike.jump-=1;
         if(bike.jump < 0){
@@ -57,8 +58,9 @@ function step(){
         }
     }
 }
+}
 step()
- setInterval(step, 200)
+ setInterval(step, 25)
 document.addEventListener('keydown', keyPressed)
 function keyPressed(e) {
     console.log("works")
